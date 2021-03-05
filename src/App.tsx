de04f7f0e702e78852e87c4632659909fdfbce9f
Axmin Shrestha <axmin@bottle.com.np>
2021-03-06 00:09:14 +0545

    playbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,75 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+
+import styled from "styled-components";
+
+const Div = styled.div`
+  .play__bar {
+    color: #fff;
+    padding: 1rem;
+    background-color: #000;
+    display: flex;
+    & .info {
+      flex: 1;
+      & h3,
+      & h6 {
+        margin: 0;
+        padding: 0;
+        text-align: left;
+      }
+      h3 {
+        font-size: 1rem;
+      }
+    }
+    & .controller__container {
+      /* background: red; */
+      flex: 10;
+      .controller__buttons {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        width: 30%;
+        margin: 0 auto;
+      }
+      .seekbar {
+        display: flex;
+        & > * {
+          flex: 1;
+        }
+      }
+    }
+  }
+`;
+const Icon = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: green;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const title = "title";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Div>
+        <div className="play__bar">
+          <div className="info">
+            <h3>{title}</h3>
+            <h6>{title}</h6>
+          </div>
+          <div className="controller__container">
+            <div className="controller__buttons">
+              <Icon />
+              <Icon />
+              <Icon />
+            </div>
+            <div className="seekbar">
+              <input type="range" />
+            </div>
+          </div>
+        </div>
+      </Div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
